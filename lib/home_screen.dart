626780eb21771b9aca907104903b1d9f911d1889
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-07-17 22:44:33 +0600

    live test 10

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,51 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_live_test_2/shoping_item_tile.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text("My Shopping List",style: TextStyle(
-              fontSize: 22
-            ),),
-            toolbarHeight: 60,
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          backgroundColor: Colors.green,
-                            content: Text("Cart is empty")));
-                  },
-                  icon: const Icon(Icons.shopping_cart,size: 26,)
-              )
-            ],
-          ),
-          body: ListView(
-            padding: const EdgeInsets.all(16),
-            shrinkWrap: true,
-            children:   const [
-              ShopingItemTile(icon:Icons.shopping_basket,text: "Apple",),
-              ShopingItemTile(icon:Icons.shopping_basket,text: "Banana",),
-              ShopingItemTile(icon:Icons.shopping_basket,text: "Milk",),
-              ShopingItemTile(icon:Icons.shopping_basket,text: "Egg",),
-              ShopingItemTile(icon:Icons.shopping_basket,text: "Bread",),
-              ShopingItemTile(icon:Icons.shopping_basket,text: "Orange",),
-
-            ],
-          )),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("AppBar"),
+      ),
+      body: const Center(
+        child: Text("Moniruzzaman"),
+      ),
     );
   }
 }
-
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_live_test_2/home_screen.dart';
 
+import 'home_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -8,12 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: HomeScreen(),
     );
   }
 }
+
